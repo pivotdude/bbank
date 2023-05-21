4e006111e3f338c26671483f21dbfd600b5aec14
pivotdude <egor.corostin@gmail.com>
2023-05-21 15:24:45 +0400

    client init

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button, Layout } from 'antd';
+
+function App() {
+  return (
+    <Layout xs={24} sm={24} md={24} lg={24}>
+      <Button>Hello</Button>
+    </Layout>
+  );
+}
+
+export default App;
