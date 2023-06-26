07c64bbf74cf09ba07c57ae94e98d8c4affa87b4
pivotdude <egor.corostin@gmail.com>
2023-06-27 01:36:00 +0400

    cors off

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: false });
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: ['*'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    allowedHeaders: ['*'],
-  });
+  // app.enableCors({
+  //   origin: ['*'],
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   credentials: true,
+  //   allowedHeaders: ['*'],
+  // });
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
